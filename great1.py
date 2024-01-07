73ccf6a145b0a9762d9749fae2cd44f8f79551e0
shahir2520 <2520shahir@gmail.com>
2024-01-07 11:34:57 +0530

    created second file

---
+++ great1.py
@@ -0,0 +1,10 @@
+a = 200
+b = 100
+if a > b :
+    print('a is greater')
+elif a == b:
+    print('both are equal')
+elif a < b:
+    print('b is greater')
+else:
+    print('all above conditions are false')
